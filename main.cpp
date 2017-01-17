3b48705205478e32db70ed6e61a10088abbf34e9
gavrielc <gabicohen22@yahoo.com>
2017-01-17 12:57:14 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /*
  * main.cpp
  *
- *  Created on: 8 αιπε 2017
+ *  Created on: 8 Γ‘Γ©Γ°Γ₯ 2017
  *      Author: Arnit
  */
 
@@ -17,14 +17,30 @@ int showPictureColor(const char* str);
 int main() {
 	SPPoint** rgbHistA;
 	SPPoint** rgbHistB;
+	SPPoint*** siftArray;
+	int* nFeatures;
+	int* res;
+	int feat, n = 2, k = 5;
 	rgbHistA=spGetRGBHist("images\\img3.png",1, 256);
-	rgbHistB=spGetRGBHist("images\\img5.png",2, 256);
+	rgbHistB=spGetRGBHist("images\\img4.png",2, 256);
 	printf("%d\n",spPointGetDimension(rgbHistA[0]));
 	printf("%f\n",spPointGetAxisCoor(rgbHistA[0],0));
 
 	printf("the dis between %f\n",spRGBHistL2Distance(rgbHistA, rgbHistB));
 	free(rgbHistA);
 	free(rgbHistB);
-	return 0;
+
+
+	siftArray = (SPPoint***) malloc(n * sizeof(SPPoint**));
+	nFeatures = (int*) malloc(n * sizeof(int));
+	siftArray[0]=spGetSiftDescriptors("images\\img3.png",0, 256, &feat);
+	nFeatures[0] = feat;
+	siftArray[1]=spGetSiftDescriptors("images\\img4.png",1, 256, &feat);
+	nFeatures[1] = feat;
+	res = spBestSIFTL2SquaredDistance(k, siftArray[1][110], siftArray, n, nFeatures);
+	printf("%d %d %d %d %d", res[0], res[1], res[2], res[3], res[4]);
+	free(siftArray);
+	free(nFeatures);
+	free(res);
 	return showPictureColor("images\\img3.png");
 }
